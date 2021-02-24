f0cf0ee1c498d33f15ca18e430357bb6cd11b11e
Mark Alldritt <alldritt@latenightsw.com>
2021-02-24 15:18:15 -0800

    Compile under macOS.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LNSBatteryLevel",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
